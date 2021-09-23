3062c0a9345bfac905bd6bdd6049f633f9cae184
Melanie Wen <melanie.a.wen@gmail.com>
2021-09-23 15:22:05 -0400

    some refactoring

--- flaskr/resize.py
+++ flaskr/resize.py
@@ -16,71 +16,91 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = 'static/uploads/'
 RESIZE_WIDTHS = [100, 300, 500, 750, 1000, 1500, 2500]
+UPLOAD_PATH = 'uploads/'
+DOWNLOAD_PATH = 'processed/'
+
+# TODO: Add functionality to delete images
+'''
+filename_dir - image path in static folder
+title - filename with extension
+name - filename without extension
+'''
 
 @bp.route('/')
-@login_required
 def gallery():
-    db = get_db()
-    images = db.execute(
-        'SELECT title, created, author_id'
-        ' FROM images i JOIN user u ON i.author_id = u.id'
-        ' WHERE u.id = ?',
-        (g.user['id'],)
-    ).fetchall()
     
-    # Convert sqlite row object to dict
-    images_dict = [dict(row) for row in images]
+    if g.user is not None:
+      # Get the images uploaded by logged in user
+      db = get_db()
+      user_images = db.execute(
+          'SELECT title, created, author_id'
+          ' FROM images i JOIN user u ON i.author_id = u.id'
+          ' WHERE u.id = ?',
+          (g.user['id'],)
+      ).fetchall()
+      
+      # Convert sqlite row object to dict
+      images = [dict(row) for row in user_images]
+      
+      for i in images:
+          i['filename_dir'] = UPLOAD_PATH + i['title']
+          
+          # Remove extension from title of image
+          i['name'] = i['title'].split(".")[-2]
     
-    for i in images_dict:
-        image_filename = 'uploads/' + i['title']
-        i['src'] = url_for('static', filename=image_filename)
-    
-    return render_template('gallery.html', images=images_dict)
+    return render_template('gallery.html', images=images)
 
-def process(sizes, filename):
-    path = APP_ROOT + '/static/' + filename
-    img = Image.open(path)
-    img_name = img.filename.split("/")[-1].strip('.jpg')
-    
+def process(sizes, filename_dir):
+    path = APP_ROOT + '/static/' + filename_dir
+    image = Image.open(path)
+    image_name, image_ext = image.filename.split("/")[-1].split('.')
+
+    # Resize images for each of the selected sizes
     for s in sizes:
-        if img.width > s:
-            downsize_pct = s/img.width
+      if image.width > s:
+        downsize_pct = s/image.width
             
-            new_width = int(img.width * downsize_pct)
-            new_height = int(img.height * downsize_pct)
-            
-            destination_dir = 'processed/' + img_name + "_" + str(s) + "w.jpg"
-            save_dir = APP_ROOT + '/static/' + destination_dir
-            
-            resized_img = img.resize((new_width, new_height))
-            resized_img.save(save_dir)
+        new_width = int(image.width * downsize_pct)
+        new_height = int(image.height * downsize_pct)
+        
+        # Have to use absolute path to save the new image
+        download_dir = APP_ROOT + '/static/' + DOWNLOAD_PATH + image_name + "_" + str(s) + "w." + image_ext
+        
+        # Resize and then save image
+        resized_image = image.resize((new_width, new_height))
+        resized_image.save(download_dir)
 
-@bp.route('/resize/<string:name>', methods=('GET', 'POST'))
+@bp.route('/resize/<string:title>', methods=('GET', 'POST'))
 @login_required
-def resize(name):
-    filename = 'uploads/' + name
+def resize(title):
+    filename_dir = UPLOAD_PATH + title
+    
+    # Append the width to the new resized files
     widths = [str(i) for i in RESIZE_WIDTHS]
 
     if request.method == 'POST':
         try:
+            # Selected sizes and convert to int
             sizes = request.form.to_dict(flat=False)['sizes']
             int_sizes = [int(i) for i in sizes]
             
-            process(int_sizes, filename)
+            process(int_sizes, filename_dir)
             
             return redirect(url_for('resize.download'))
         except:
-            flash('something went wrong')
+            flash('Something went wrong. Please try again.')
     
-    return render_template('resize.html', filename=filename, resize_widths=widths)
+    return render_template('resize.html', filename_dir=filename_dir, resize_widths=widths)
 
-def allowed_file(filename):
-  return '.' in filename and \
-    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_file(title):
+  return '.' in title and \
+    title.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bp.route('/upload', methods=('GET', 'POST'))
 @login_required
-def upload_file():
+def upload():
+  # TODO: Check for duplicate filenames for user
+  
   if request.method == 'POST':
     file = request.files['file']
     error = None
@@ -106,8 +126,7 @@ def upload_file():
         )
       db.commit()
       
-      # TODO: Remove this eventually 
-      flash('SUCCESS!!!!!!!!!')
+      return redirect(url_for('resize.gallery'))
   
   return render_template('upload.html')
 
